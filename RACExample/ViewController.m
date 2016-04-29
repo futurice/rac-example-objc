2d83d5c8c2a197fc66877f81ff986a737956d122
Martin Richter <martin.richter@futurice.com>
2016-04-29 16:10:10 +0200

    Set up basic signal chain from text field to label

--- RACExample/ViewController.m
+++ RACExample/ViewController.m
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Futurice. All rights reserved.
 //
 
+#import <ReactiveCocoa/ReactiveCocoa.h>
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -13,5 +14,24 @@
 @end
 
 @implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Creating Observables (called Signals in RAC)
+    RACSignal *textSignal = self.textField.rac_textSignal;
+
+    // Applying basic operators
+    RACSignal *uppercaseTextSignal = [[textSignal map:^(NSString *text) {
+        return text.uppercaseString;
+    }]
+    // Debugging signals
+    doNext:^(NSString *text) {
+        NSLog(@"Got text: %@", text);
+    }];
+
+    // Creating UI bindings
+    RAC(self.label, text) = uppercaseTextSignal;
+}
 
 @end
