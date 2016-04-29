6d07af3313daaa6e15d32029f8ced294fba5a201
Martin Richter <martin.richter@futurice.com>
2016-04-29 11:21:12 +0200

    Initial commit

---
+++ RACExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RACExample
+//
+//  Created by Martin Richter on 29/04/16.
+//  Copyright © 2016 Futurice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RACExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RACExample
+//
+//  Created by Martin Richter on 29/04/16.
+//  Copyright © 2016 Futurice. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
