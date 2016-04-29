e3ef127303b16cca843b19e3cb90460d5b69c82b
Martin Richter <me@martinrichter.net>
2016-04-29 18:07:27 +0200

    Allow switching between text and date values in label using segmented control

--- RACExample/ViewController.m
+++ RACExample/ViewController.m
@@ -18,20 +18,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    // Creating Observables (called Signals in RAC)
+    /* Creating Observables (called signals in RAC) */
+
+    // Get signal that sends latest text field values
     RACSignal *textSignal = self.textField.rac_textSignal;
 
-    // Applying basic operators
+    /*  Applying basic operators, debugging signals */
+
     RACSignal *uppercaseTextSignal = [[textSignal map:^(NSString *text) {
-        return text.uppercaseString;
+        return text.uppercaseString; // transform values
     }]
-    // Debugging signals
     doNext:^(NSString *text) {
-        NSLog(@"Got text: %@", text);
+        NSLog(@"Got text: %@", text); // useful for debugging
     }];
 
-    // Creating UI bindings
-    RAC(self.label, text) = uppercaseTextSignal;
+    /* Switching between value streams using a signal of signals */
+
+    // Create signal that sends current date every second
+    RACSignal *dateSignal = [[RACSignal interval:1 onScheduler:[RACScheduler mainThreadScheduler]] replayLast];
+
+    RACSignal *dateTextSignal = [dateSignal map:^(NSDate *date) {
+        return [NSDateFormatter localizedStringFromDate:date
+            dateStyle:NSDateFormatterMediumStyle
+            timeStyle:NSDateFormatterMediumStyle];
+    }];
+
+    // Get signal that sends state of segmented control
+    RACSignal *controlSignal = [[self.segmentedControl
+        rac_newSelectedSegmentIndexChannelWithNilValue:@0]
+        startWith:@0];
+
+    RACSignal *labelTextSignal = [[controlSignal
+        map:^(NSNumber *index) {
+            NSUInteger indexValue = index.unsignedIntegerValue;
+            switch (indexValue) {
+                case 0:
+                    return uppercaseTextSignal;
+                case 1:
+                    return dateTextSignal;
+                default:
+                    return [RACSignal empty];
+            }
+        }]
+        switchToLatest];
+
+    /* Creating UI bindings */
+
+    RAC(self.label, text) = labelTextSignal;
 }
 
 @end
