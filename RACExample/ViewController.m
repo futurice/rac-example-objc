04d89c26eca23281a6947bd0223af8450a9eb4f7
Martin Richter <me@martinrichter.net>
2016-05-02 12:23:46 +0200

    Remove solutions from code

--- RACExample/ViewController.m
+++ RACExample/ViewController.m
@@ -24,72 +24,33 @@
 
     /* [1] Create an Observable (called Signal in RAC) */
 
-    // TODO: Create RACSignal and send some values, printing them to console
+    // RACSignal *signal = ...
 
-    /* [2a] Get a signal sending the latest values of a text field */
+    /* [2] Get a signal sending the latest values of the text field */
 
     // RACSignal *textSignal = ...
 
-    RACSignal *textSignal = self.textField.rac_textSignal;
-
-    /* [3] Transform the text values using basic operators */
-    /* [4] Debug the signal by logging its values without subscribing */
+    /* [4] Manipulate the text signal using basic operators */
+    /* [5] Debug the signal by logging its values without subscribing */
 
     // RACSignal *uppercaseTextSignal = ...
 
-    RACSignal *uppercaseTextSignal = [[textSignal map:^(NSString *text) {
-        return text.uppercaseString; // transform values
-    }]
-    doNext:^(NSString *text) {
-        NSLog(@"Got text: %@", text); // useful for debugging
-    }];
-
-    /* [5] Switching between value streams using a signal of signals */
-    /* [6] Cache latest date value to avoid pause when switching to date signal */
+    /* [6] Switch between value streams using a signal of signals */
+    /* [7] Cache latest date value to avoid pause when switching to date signal */
 
     // Create a signal that sends the current date every second
     // RACSignal *dateSignal = ...
 
-    RACSignal *dateSignal = [[RACSignal
-        interval:1 onScheduler:[RACScheduler mainThreadScheduler]]
-        replayLast]; // This implements [6]
-
     // Convert the dates to formatted date strings
     // RACSignal *dateStringSignal = ...
 
-    RACSignal *dateStringSignal = [dateSignal map:^(NSDate *date) {
-        return [NSDateFormatter localizedStringFromDate:date
-            dateStyle:NSDateFormatterMediumStyle
-            timeStyle:NSDateFormatterMediumStyle];
-    }];
-
-    // Get signal that sends state of segmented control
+    // Get a signal that sends the segmented control's state
     // RACSignal *controlSignal = ...
-
-    RACSignal *controlSignal = [[self.segmentedControl
-        rac_newSelectedSegmentIndexChannelWithNilValue:@0]
-        startWith:@0];
 
     // Get signal that switches between field text and date string values
     // RACSignal *labelTextSignal = ...
 
-    RACSignal *labelTextSignal = [[controlSignal
-        map:^(NSNumber *index) {
-            NSUInteger indexValue = index.unsignedIntegerValue;
-            switch (indexValue) {
-                case 0:
-                    return uppercaseTextSignal;
-                case 1:
-                    return dateStringSignal;
-                default:
-                    return [RACSignal empty];
-            }
-        }]
-        switchToLatest];
-
-    /* [2b] Bind text of label to whatever the final signal sends */
-
-    RAC(self.label, text) = labelTextSignal;
+    /* [3] Bind text of label to whatever the final signal sends */
 }
 
 - (void)viewWillAppear:(BOOL)animated {
--- README.md
+++ README.md
@@ -11,9 +11,11 @@ ReactiveCocoa (RAC) is a powerful framework for working with streams of values o
 
 Observables are called _signals_ in RAC. You can create one using `RACSignal`'s class method `createSignal`, which gives you a block from which you can send values and other events to subscribers. You need to return a `RACDisposable`, which allows you to clean up things once a subscription ends.
 
-### 2. Bind the text field's latest value to the label
+### 2. Get a signal that sends the text field's latest values
 
 RAC comes with a range of categories on UIKit, which let you access properties such as `UITextField` values as streams. Every time the value changes, the stream sends a `Next` event carrying that value. These category methods all start with the `rac_` prefix.
+
+### 3. Bind the text signal to the label
 
 Once you have a signal that sends the text field's latest values, you can bind it to the label using the `RAC` macro:
 
@@ -23,17 +25,17 @@ RAC(self.label, text) = textSignal;
 
 This also automatically terminates the subscription once the label gets deallocated.
 
-### 3. Manipulate the text signal using basic operators
+### 4. Manipulate the text signal using basic operators
 
 These operators include `map` for transforming values, and `filter` for skipping values based on a condition.
 
 For instance, try converting the text field values to uppercase before displaying them in the label.
 
-### 4. Debug the signal
+### 5. Debug the signal
 
 RAC comes with useful logging features, such as the `doNext` operator. It lets us print the values to the console in a non-invasive fashion (without subscribing to the signal). Note that a subscription is still required at the end of the chain, or you won't see anything!
 
-### 5. Switch between value streams using a signal of signals
+### 6. Switch between value streams using a signal of signals
 
 The aim is to let the user control what they see in the label, using the segmented control. Depending on their choice, either the latest text field input or the current date is shown.
 
@@ -43,6 +45,6 @@ The aim is to let the user control what they see in the label, using the segment
 1. Map the control values (`0` or `1` for the first and second segment, respectively) to the signals you want to switch between.
 1. Use the `switchToLatest` operator to forward only values from latest returned "inner" signal.
 
-### 6. Replay the latest value when switching to the date signal
+### 7. Replay the latest value when switching to the date signal
 
 The date signal sends a value every second, meaning that the label text won't update immediately when the user switches to it. We can fix this by appending `replayLast` to the date signal, which causes it to send its last value to new subscribers immediately upon subscription.
